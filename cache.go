b6c992f584aa50dabb394a0fb459a9bee3af320d
romixweb <104103605+romixweb@users.noreply.github.com>
2023-05-03 01:37:53 +0300

    Create cache.go

---
+++ cache.go
@@ -0,0 +1,23 @@
+package cache
+
+type Cache struct {
+	data map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{
+		data: make(map[string]interface{}),
+	}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	(*c).data[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return (*c).data[key]
+}
+
+func (c *Cache) Delete(key string) {
+	delete((*c).data, key)
+}
